6bd202b0af9f4d9d7a9b921f2f42c8319a5fa9b3
Olivier <olivier.castille@gmail.com>
2024-12-10 17:28:39 +0100

    feat(streaming): handle proxy errors

---
+++ Sources/OpenAI/Public/Errors/ProxyError.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  OpenAI
+//
+//  Created by Olivier on 10/12/2024.
+//
+
+import Foundation
+
+struct ProxyErrorResponse: Decodable {
+    
+    // Attributes
+    let fault: Fault
+    
+    // Models
+    struct Fault: Decodable {
+        let faultstring: String
+        let detail: Detail
+    }
+    
+    struct Detail: Decodable {
+        let errorcode: String
+        
+        // Helpers
+        var asError: ProxyError {
+            .init(rawValue: errorcode) ?? .unkonwn
+        }
+    }
+}
+
+// Models
+enum ProxyError: String, Error {
+    case invalidToken = "steps.jwt.InvalidToken"
+    case unkonwn
+}
